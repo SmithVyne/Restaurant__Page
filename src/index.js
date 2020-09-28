6d8387be91c54fb3512b6f5fb042c9d7cfbe1e1e
SmithVyne <smithnkereuwem2@gmail.com>
2020-09-28 20:11:07 +0300

    Finish restaurant website

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello Webpack from Sitsilya");
+import navBar from './navbar';
+import { header, pageBody } from './majors';
+
+const loadContent = () => {
+  const contentBox = document.querySelector('#content');
+  header(contentBox);
+  navBar(contentBox);
+  pageBody(contentBox);
+};
+
+loadContent();
---
+++ src/majors.js
@@ -0,0 +1,52 @@
+const header = (contentBox) => {
+  contentBox.innerHTML = `<h1 id="header">
+  A Great Restaurant
+</h1>`;
+};
+
+const pageBody = (contentBox) => {
+  contentBox.innerHTML += `<div class="tab-content" id="nav-tabContent">
+ <div class="tab-pane fade show active p-2" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+   <div id="home-img"></div>
+   <p>
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus blanditiis in fuga corporis, itaque, natus nihil commodi dolorem beatae aliquid dolor magni quasi atque cumque. Consequatur fugit iste sint nemo!
+   </p>
+ </div>
+
+ <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+   <div class="menu-items">
+     <div class="menu-itm">
+       <div class="menu-img"></div>
+       <span class="d-block">Pancakes</span>
+     </div>
+     <div class="menu-itm">
+       <div class="menu-img"></div>
+       <span class="d-block">IceCream</span>
+     </div>
+     <div class="menu-itm">
+       <div class="menu-img"></div>
+       <span class="d-block">Candy</span>
+     </div>
+   </div>
+ </div>
+
+ <div class="tab-pane fade p-4" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+   <form id="contact-form">
+     <div class="d-flex">
+       <div class="labels mr-2">
+         <div>Your Request: </div>
+         <div class="mt-3">Email here: </div>
+       </div>
+       <div class="entries">
+         <input type="text" id="request-box">
+         <input type="email" id="email-box" class="mt-2">
+       </div>
+     </div>
+     <button type="submit" class="mt-3 btn btn-primary">Submit</button>
+   </form>
+ </div>
+</div>`;
+};
+
+
+export { header, pageBody };
